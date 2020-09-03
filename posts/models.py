e47c755eb43001e157c798f172e6388f8f4845c4
unknown <tukavka38@gmail.com>
2020-09-03 12:11:05 +0300

    Add Post and Comment models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author_name = models.ForeignKey(
+        User,
+        related_name='posts',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    title = models.CharField(max_length=120)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    link = models.URLField()
+    upvotes = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author_name = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    parent = models.ForeignKey('Comment', related_name='replies', on_delete=models.CASCADE, null=True, default=None)
+    content = models.TextField(null=True)
